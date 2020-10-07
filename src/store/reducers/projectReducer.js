cbef5d950afe4a812af77d26544ee621fe09372c
Ahmed Amir Salem (Kenway The Great) <ahmad_amir@live.com>
2020-10-07 20:56:22 +0200

    Will install dependencies and style components.

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+  projects: [
+    { id: 1, title: "test", content: "just a first test" },
+    { id: 2, title: "test 2", content: "just a second test" },
+    { id: 3, title: "test 3", content: "just a third test" },
+  ],
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
